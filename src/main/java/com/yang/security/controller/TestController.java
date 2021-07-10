69f4cebeef27d38ded505ecec4396e6d8945ce08
Tommy <814230847@qq.com>
2021-07-11 07:51:29 +0800

    feat:杂七杂八测权限

--- src/main/java/com/yang/security/controller/TestController.java
+++ src/main/java/com/yang/security/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.yang.security.controller;
 
 import com.yang.security.utils.Result;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
     @PostMapping("/test")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public Result test(){
         return Result.success("test");
     }
 
     @PostMapping("/test2")
+    @PreAuthorize("hasPermission('login:select')")
     public Result test2(){
         return Result.success("test");
     }
